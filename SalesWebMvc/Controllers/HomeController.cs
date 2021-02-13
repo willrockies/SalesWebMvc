9612ab49829548419f6e378ad4d254d32f856893
Wilson Alves <wlsn.ads@gmail.com>
2021-02-12 22:19:36 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web mvc in C#";
+            ViewData["Developer"] = "Wilson Alves da Silva";
 
             return View();
         }
