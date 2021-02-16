3052ef24b6e8cf3499a7cc988b0d20309accd6e8
Wilson Alves <wlsn.ads@gmail.com>
2021-02-16 16:54:26 -0300

    Foreign key not null - referential integrity

---
+++ SalesWebMvc/Migrations/20210216195043_DepartmentForeignKey.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Salary",
+                table: "Seller",
+                newName: "BaseSalary");
+
+            migrationBuilder.RenameColumn(
+                name: "Birthday",
+                table: "Seller",
+                newName: "BirthDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Seller",
+                newName: "Birthday");
+
+            migrationBuilder.RenameColumn(
+                name: "BaseSalary",
+                table: "Seller",
+                newName: "Salary");
+        }
+    }
+}
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -10,20 +10,21 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime Birthday { get; set; }
-        public double Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
 
-        public Seller(int id, string name, string email, DateTime birthday, double salary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            Birthday = birthday;
-            Salary = salary;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
 
